4548f451bd6a4999a7a51851a23630ff23dd6c0b
Shukazuby <shukazuby@gmail.com>
2025-06-17 15:55:21 +0100

    removed cron

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -173,63 +173,6 @@ export class NotificationService {
 //   }
 // }
 
-async sendInactivityNotification() {
-  try {
-    const users = await this.userModel.find();
-
-    const now = Date.now();
-    const oneDayAgo = new Date(now - 1 * 24 * 60 * 60 * 1000);
-    const twoDaysAgo = new Date(now - 2 * 24 * 60 * 60 * 1000);
-    const fourDaysAgo = new Date(now - 4 * 24 * 60 * 60 * 1000);
-    const fiveDaysAgo = new Date(now - 5 * 24 * 60 * 60 * 1000);
-    const nineDaysAgo = new Date(now - 9 * 24 * 60 * 60 * 1000);
-    const tenDaysAgo = new Date(now - 10 * 24 * 60 * 60 * 1000);
-
-    for (const user of users) {
-      const latestHotTake = await this.hotModel
-        .findOne({ sender: user.username })
-        .sort({ createdAt: -1 });
-
-      const lastPostDate = latestHotTake ? latestHotTake.createdAt : new Date(0);
-
-      // Inactivity 1: Between 2 and 1 days ago
-      if (!user.isInactive1 && lastPostDate <= twoDaysAgo && lastPostDate > oneDayAgo) {
-        await this.sendNotificationToUser(
-          user,
-          "The timeline’s too quiet without you. Drop that hot take 🔥👀",
-          "Where You At?"
-        );
-        user.isInactive1 = true;
-        await user.save();
-      }
-
-      // Inactivity 2: Between 5 and 4 days ago
-      else if (!user.isInactive2 && lastPostDate <= fiveDaysAgo && lastPostDate > fourDaysAgo) {
-        await this.sendNotificationToUser(
-          user,
-          "No hot takes, yet? Wow, groundbreaking silence.",
-          "Still Cooking?"
-        );
-        user.isInactive2 = true;
-        await user.save();
-      }
-
-      // Inactivity 3: Between 10 and 9 days ago
-      else if (!user.isInactive3 && lastPostDate <= tenDaysAgo && lastPostDate > nineDaysAgo) {
-        await this.sendNotificationToUser(
-          user,
-          "Been 10 days since your last hot take... just sayin",
-          "Your Feed Needs You"
-        );
-        user.isInactive3 = true;
-        await user.save();
-      }
-    }
-  } catch (error) {
-    console.error("Error sending inactivity notifications:", error);
-    throw error;
-  }
-}
 
 private async sendNotificationToUser(user: any, message: string, title: string) {
   const payload = {
@@ -247,4 +190,66 @@ private async sendNotificationToUser(user: any, message: string, title: string) 
   await this.createNotifiction(payload);
   await sendPushNotification(message, user.token, title);
 }
+
+//validddddddddddd
+
+
+// async sendInactivityNotification() {
+//   try {
+//     const users = await this.userModel.find();
+
+//     const now = Date.now();
+//     const oneDayAgo = new Date(now - 1 * 24 * 60 * 60 * 1000);
+//     const twoDaysAgo = new Date(now - 2 * 24 * 60 * 60 * 1000);
+//     const fourDaysAgo = new Date(now - 4 * 24 * 60 * 60 * 1000);
+//     const fiveDaysAgo = new Date(now - 5 * 24 * 60 * 60 * 1000);
+//     const nineDaysAgo = new Date(now - 9 * 24 * 60 * 60 * 1000);
+//     const tenDaysAgo = new Date(now - 10 * 24 * 60 * 60 * 1000);
+
+//     for (const user of users) {
+//       const latestHotTake = await this.hotModel
+//         .findOne({ sender: user.username })
+//         .sort({ createdAt: -1 });
+
+//       const lastPostDate = latestHotTake ? latestHotTake.createdAt : new Date(0);
+
+//       // Inactivity 1: Between 2 and 1 days ago
+//       if (!user.isInactive1 && lastPostDate <= twoDaysAgo && lastPostDate > oneDayAgo) {
+//         await this.sendNotificationToUser(
+//           user,
+//           "The timeline’s too quiet without you. Drop that hot take 🔥👀",
+//           "Where You At?"
+//         );
+//         user.isInactive1 = true;
+//         await user.save();
+//       }
+
+//       // Inactivity 2: Between 5 and 4 days ago
+//       else if (!user.isInactive2 && lastPostDate <= fiveDaysAgo && lastPostDate > fourDaysAgo) {
+//         await this.sendNotificationToUser(
+//           user,
+//           "No hot takes, yet? Wow, groundbreaking silence.",
+//           "Still Cooking?"
+//         );
+//         user.isInactive2 = true;
+//         await user.save();
+//       }
+
+//       // Inactivity 3: Between 10 and 9 days ago
+//       else if (!user.isInactive3 && lastPostDate <= tenDaysAgo && lastPostDate > nineDaysAgo) {
+//         await this.sendNotificationToUser(
+//           user,
+//           "Been 10 days since your last hot take... just sayin",
+//           "Your Feed Needs You"
+//         );
+//         user.isInactive3 = true;
+//         await user.save();
+//       }
+//     }
+//   } catch (error) {
+//     console.error("Error sending inactivity notifications:", error);
+//     throw error;
+//   }
+// }
+
 }
--- src/utils/Cron/cron.ts
+++ src/utils/Cron/cron.ts
@@ -6,18 +6,18 @@ import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class CronWork {
-  constructor(
-    private readonly usrSrv: UsersService,
-    private readonly notSrv: NotificationService
-  ) {
-    this.scheduleJobs();
-  }
+  // constructor(
+  //   private readonly usrSrv: UsersService,
+  //   private readonly notSrv: NotificationService
+  // ) {
+  //   this.scheduleJobs();
+  // }
 
-  private scheduleJobs() {
-    cron.schedule('* * * * *', async () => {
-      console.log('🚀 Running scheduled job ...');
-     await this.notSrv.sendInactivityNotification();
-      console.log('created ...');
-    });
-  } 
+  // private scheduleJobs() {
+  //   cron.schedule('* * * * *', async () => {
+  //     console.log('🚀 Running scheduled job ...');
+  //    await this.notSrv.sendInactivityNotification();
+  //     console.log('created ...');
+  //   });
+  // } 
 }
